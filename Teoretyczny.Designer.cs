d5dbf01b82b69a90bb4f8822ce15ba706bf94475
Jakub Konicki <147485931+HoverKK@users.noreply.github.com>
2024-05-20 19:19:47 +0200

    Utworzenie egzaminu teoretycznego

---
+++ Teoretyczny.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace prawo_jazdy
+{
+    partial class Teoretyczny
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label labelQuestion;
+        private System.Windows.Forms.RadioButton radioButtonA;
+        private System.Windows.Forms.RadioButton radioButtonB;
+        private System.Windows.Forms.RadioButton radioButtonC;
+        private System.Windows.Forms.Button buttonCheck;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.Label labelTime;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            labelQuestion = new Label();
+            radioButtonA = new RadioButton();
+            radioButtonB = new RadioButton();
+            radioButtonC = new RadioButton();
+            buttonNext = new Button();
+            labelTime = new Label();
+            SuspendLayout();
+            // 
+            // labelQuestion
+            // 
+            labelQuestion.Location = new Point(13, 13);
+            labelQuestion.Name = "labelQuestion";
+            labelQuestion.Size = new Size(775, 23);
+            labelQuestion.TabIndex = 0;
+            labelQuestion.Text = "labelQuestion";
+            // 
+            // radioButtonA
+            // 
+            radioButtonA.Location = new Point(16, 50);
+            radioButtonA.Name = "radioButtonA";
+            radioButtonA.Size = new Size(772, 24);
+            radioButtonA.TabIndex = 1;
+            radioButtonA.TabStop = true;
+            radioButtonA.Text = "radioButtonA";
+            radioButtonA.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonB
+            // 
+            radioButtonB.Location = new Point(16, 80);
+            radioButtonB.Name = "radioButtonB";
+            radioButtonB.Size = new Size(772, 24);
+            radioButtonB.TabIndex = 2;
+            radioButtonB.TabStop = true;
+            radioButtonB.Text = "radioButtonB";
+            radioButtonB.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonC
+            // 
+            radioButtonC.Location = new Point(16, 110);
+            radioButtonC.Name = "radioButtonC";
+            radioButtonC.Size = new Size(772, 24);
+            radioButtonC.TabIndex = 3;
+            radioButtonC.TabStop = true;
+            radioButtonC.Text = "radioButtonC";
+            radioButtonC.UseVisualStyleBackColor = true;
+            // 
+            // buttonNext
+            // 
+            buttonNext.Location = new Point(16, 140);
+            buttonNext.Name = "buttonNext";
+            buttonNext.Size = new Size(82, 35);
+            buttonNext.TabIndex = 5;
+            buttonNext.Text = "Następne";
+            buttonNext.UseVisualStyleBackColor = true;
+            buttonNext.Click += buttonNext_Click;
+            // 
+            // labelTime
+            // 
+            labelTime.Location = new Point(12, 190);
+            labelTime.Name = "labelTime";
+            labelTime.Size = new Size(772, 23);
+            labelTime.TabIndex = 6;
+            labelTime.Text = "Pozostały czas: 30 sekund";
+            labelTime.Click += Timer_Tick;
+            // 
+            // Teoretyczny
+            // 
+            ClientSize = new Size(800, 287);
+            Controls.Add(labelTime);
+            Controls.Add(buttonNext);
+            Controls.Add(radioButtonC);
+            Controls.Add(radioButtonB);
+            Controls.Add(radioButtonA);
+            Controls.Add(labelQuestion);
+            Name = "Teoretyczny";
+            Load += Teoretyczny_Load;
+            ResumeLayout(false);
+        }
+    }
+}
---
+++ Teoretyczny.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace prawo_jazdy
+{
+    public partial class Teoretyczny : Form
+    {
+        private int currentQuestionIndex = 0;
+        private int timeLeft = 30;
+        private System.Windows.Forms.Timer timer;
+        private int correctAnswersCount = 0;
+        private List<Question> questions = new List<Question>();
+
+        private string connectionString = "your_connection_string_here"; // Uzupełnij odpowiednimi danymi
+
+        public Teoretyczny()
+        {
+            InitializeComponent();
+            LoadQuestionsFromDatabase();
+            InitializeTimer();
+            DisplayQuestion();
+        }
+
+        private void LoadQuestionsFromDatabase()
+        {
+            using (SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-ED41F2S;Initial Catalog=Prawo_jazdy;Integrated Security=True"))
+            {
+                connection.Open();
+                string query = "SELECT p.PytanieID, p.PytanieText, o.OdpowiedzID, o.OdpowiedzText, o.Poprawne " +
+                               "FROM Pytania p " +
+                               "JOIN Odpowiedzi o ON p.PytanieID = o.PytanieID";
+
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                Dictionary<int, Question> questionDictionary = new Dictionary<int, Question>();
+
+                while (reader.Read())
+                {
+                    int questionId = reader.GetInt32(0);
+                    string questionText = reader.GetString(1);
+                    int answerId = reader.GetInt32(2);
+                    string answerText = reader.GetString(3);
+                    bool isCorrect = reader.GetBoolean(4);
+
+                    if (!questionDictionary.ContainsKey(questionId))
+                    {
+                        questionDictionary[questionId] = new Question
+                        {
+                            QuestionID = questionId,
+                            QuestionText = questionText,
+                            Answers = new List<Answer>()
+                        };
+                    }
+
+                    questionDictionary[questionId].Answers.Add(new Answer
+                    {
+                        AnswerID = answerId,
+                        AnswerText = answerText,
+                        IsCorrect = isCorrect
+                    });
+                }
+
+                questions.AddRange(questionDictionary.Values);
+                reader.Close();
+            }
+        }
+
+        private void InitializeTimer()
+        {
+            timer = new System.Windows.Forms.Timer();
+            timer.Interval = 1000; // 1 sekunda
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (timeLeft > 0)
+            {
+                timeLeft--;
+                labelTime.Text = $"Pozostały czas: {timeLeft} sekund";
+            }
+            else
+            {
+                timer.Stop();
+                CheckAnswer();
+                GoToNextQuestion();
+            }
+        }
+
+        private void DisplayQuestion()
+        {
+            timeLeft = 30; // Resetowanie czasu dla każdego pytania
+            labelTime.Text = $"Pozostały czas: {timeLeft} sekund";
+            timer.Start();
+
+            var currentQuestion = questions[currentQuestionIndex];
+            labelQuestion.Text = $"{currentQuestionIndex + 1}. {currentQuestion.QuestionText}";
+            radioButtonA.Text = currentQuestion.Answers[0].AnswerText;
+            radioButtonB.Text = currentQuestion.Answers[1].AnswerText;
+            radioButtonC.Text = currentQuestion.Answers[2].AnswerText;
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            CheckAnswer();
+            GoToNextQuestion();
+        }
+
+        private void CheckAnswer()
+        {
+            char selectedAnswer = ' ';
+            if (radioButtonA.Checked)
+                selectedAnswer = 'a';
+            else if (radioButtonB.Checked)
+                selectedAnswer = 'b';
+            else if (radioButtonC.Checked)
+                selectedAnswer = 'c';
+
+            var correctAnswer = questions[currentQuestionIndex].Answers.Find(a => a.IsCorrect);
+            if (correctAnswer != null && correctAnswer.AnswerText.StartsWith(selectedAnswer.ToString()))
+            {
+                correctAnswersCount++; // Zwiększenie liczby poprawnych odpowiedzi
+            }
+
+            // Wyczyść zaznaczenie przed kolejnym pytaniem
+            radioButtonA.Checked = false;
+            radioButtonB.Checked = false;
+            radioButtonC.Checked = false;
+        }
+
+        private void GoToNextQuestion()
+        {
+            if (currentQuestionIndex < questions.Count - 1)
+            {
+                currentQuestionIndex++;
+                DisplayQuestion();
+            }
+            else
+            {
+                timer.Stop();
+                var wynikForm = new Wynik(correctAnswersCount, questions.Count);
+                wynikForm.Show();
+                this.Close();
+            }
+        }
+
+        private void Teoretyczny_Load(object sender, EventArgs e)
+        {
+            DisplayQuestion();
+        }
+    }
+
+    public class Question
+    {
+        public int QuestionID { get; set; }
+        public string QuestionText { get; set; }
+        public List<Answer> Answers { get; set; }
+    }
+
+    public class Answer
+    {
+        public int AnswerID { get; set; }
+        public string AnswerText { get; set; }
+        public bool IsCorrect { get; set; }
+    }
+}
