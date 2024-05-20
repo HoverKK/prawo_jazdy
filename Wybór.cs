5d69e67ee5a2d2154c7e03720540f9751562af39
Jakub Konicki <147485931+HoverKK@users.noreply.github.com>
2024-05-20 19:26:46 +0200

    Dodanie wyboru egzaminu
    teoretycznego i praktycznego

---
+++ Wybór.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prawo_jazdy
+{
+    public partial class Wybór : Form
+    {
+        public Wybór()
+        {
+            InitializeComponent();
+        }
+
+        private void Wybór_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Teoretyczny form = new Teoretyczny();
+            form.Show();
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Praktyczny form = new Praktyczny();
+            form.Show();
+            this.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Menuform form1 = new Menuform();
+            form1.Show();
+            this.Hide();
+        }
+    }
+}
