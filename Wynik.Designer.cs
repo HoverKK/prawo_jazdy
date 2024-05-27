9eb2e347421590b264a0698be5c67900774b88e2
MaxenWolf <147586449+MaxenWolf@users.noreply.github.com>
2024-05-27 19:29:09 +0200

    Dodanie okna z wynikami
    Dodanie okna z wynikami do egzaminu praktycznego

---
+++ Wynik.Designer.cs
@@ -0,0 +1,40 @@
+﻿namespace prawo_jazdy
+{
+    partial class Wynik
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label labelResult;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            labelResult = new Label();
+            SuspendLayout();
+            // 
+            // labelResult
+            // 
+            labelResult.AutoSize = true;
+            labelResult.Location = new Point(12, 9);
+            labelResult.Name = "labelResult";
+            labelResult.Size = new Size(50, 20);
+            labelResult.TabIndex = 0;
+            labelResult.Text = "label1";
+            // 
+            // Wynik
+            // 
+            ClientSize = new Size(685, 443);
+            Controls.Add(labelResult);
+            Name = "Wynik";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+    }
+}
---
+++ Wynik.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System;
+using System.Windows.Forms;
+
+namespace prawo_jazdy
+{
+    public partial class Wynik : Form
+    {
+        public Wynik(int correctAnswers, int totalQuestions)
+        {
+            InitializeComponent();
+            labelResult.Text = $"Liczba poprawnych odpowiedzi: {correctAnswers}/{totalQuestions}";
+        }
+    }
+}
