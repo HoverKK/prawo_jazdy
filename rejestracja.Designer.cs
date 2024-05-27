dc854ee9d20003b10f7c3963da76109b9c93765d
MaxenWolf <147586449+MaxenWolf@users.noreply.github.com>
2024-05-27 18:43:52 +0200

    Dodanie rejestracji
    dodanie rejestracji do programu

---
+++ rejestracja.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace prawo_jazdy
+{
+    partial class rejestracja
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button2 = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // button2
+            // 
+            button2.Location = new Point(12, 409);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 1;
+            button2.Text = "Cofnij";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            label1.Location = new Point(330, 95);
+            label1.Name = "label1";
+            label1.Size = new Size(170, 41);
+            label1.TabIndex = 2;
+            label1.Text = "Rejestracja";
+            // 
+            // rejestracja
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Name = "rejestracja";
+            Text = "rejestracja";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Button button2;
+        private Label label1;
+    }
+}
---
+++ rejestracja.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace prawo_jazdy
+{
+    public partial class rejestracja : Form
+    {
+        private TextBox txtImię;
+        private TextBox txtNazwisko;
+        private TextBox txtEmail;
+        private TextBox txtHasło;
+
+        private SqlConnection conn;
+
+        public rejestracja()
+        {
+            InitializeComponent();
+
+            conn = new SqlConnection(@"Data Source=DESKTOP-ED41F2S;Initial Catalog=Prawo_jazdy;Integrated Security=True");
+
+            InitializeControls();
+        }
+
+        private void InitializeControls()
+        {
+            Label lblImię = new Label();
+            lblImię.Text = "Imię:";
+            lblImię.Location = new Point(this.Width / 2 - 150, this.Height / 2 - 80);
+            this.Controls.Add(lblImię);
+
+            txtImię = new TextBox();
+            txtImię.Location = new Point(this.Width / 2 - 50, this.Height / 2 - 80);
+            txtImię.Size = new Size(100, 20);
+            this.Controls.Add(txtImię);
+
+            Label lblNazwisko = new Label();
+            lblNazwisko.Text = "Nazwisko:";
+            lblNazwisko.Location = new Point(this.Width / 2 - 150, this.Height / 2 - 50);
+            this.Controls.Add(lblNazwisko);
+
+            txtNazwisko = new TextBox();
+            txtNazwisko.Location = new Point(this.Width / 2 - 50, this.Height / 2 - 50);
+            txtNazwisko.Size = new Size(100, 20);
+            this.Controls.Add(txtNazwisko);
+
+            Label lblEmail = new Label();
+            lblEmail.Text = "E-mail:";
+            lblEmail.Location = new Point(this.Width / 2 - 150, this.Height / 2 - 20);
+            this.Controls.Add(lblEmail);
+
+            txtEmail = new TextBox();
+            txtEmail.Location = new Point(this.Width / 2 - 50, this.Height / 2 - 20);
+            txtEmail.Size = new Size(100, 20);
+            this.Controls.Add(txtEmail);
+
+            Label lblHasło = new Label();
+            lblHasło.Text = "Hasło:";
+            lblHasło.Location = new Point(this.Width / 2 - 150, this.Height / 2 + 10);
+            this.Controls.Add(lblHasło);
+
+            txtHasło = new TextBox();
+            txtHasło.Location = new Point(this.Width / 2 - 50, this.Height / 2 + 10);
+            txtHasło.Size = new Size(100, 20);
+            txtHasło.PasswordChar = '*';
+            this.Controls.Add(txtHasło);
+
+            Button buttonZarejestruj = new Button();
+            buttonZarejestruj.Text = "Zarejestruj";
+            buttonZarejestruj.Location = new Point(this.Width / 2 - 50, this.Height / 2 + 40);
+            buttonZarejestruj.Size = new Size(100, 30);
+            buttonZarejestruj.Click += new EventHandler(buttonZarejestruj_Click);
+            this.Controls.Add(buttonZarejestruj);
+        }
+
+        private void buttonZarejestruj_Click(object sender, EventArgs e)
+        {
+            string imię = txtImię.Text;
+            string nazwisko = txtNazwisko.Text;
+            string email = txtEmail.Text;
+            string hasło = txtHasło.Text;
+
+            if (string.IsNullOrWhiteSpace(imię) || string.IsNullOrWhiteSpace(nazwisko) || string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(hasło))
+            {
+                MessageBox.Show("Wszystkie pola są wymagane.", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                conn.Open();
+
+                string query = "INSERT INTO urzytkownik (imię, nazwisko, email, hasło) VALUES (@imię, @nazwisko, @email, @hasło)";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@imię", imię);
+                cmd.Parameters.AddWithValue("@nazwisko", nazwisko);
+                cmd.Parameters.AddWithValue("@email", email);
+                cmd.Parameters.AddWithValue("@hasło", hasło);
+
+                int rowsAffected = cmd.ExecuteNonQuery();
+
+                if (rowsAffected > 0)
+                {
+                    MessageBox.Show("Rejestracja zakończona sukcesem.", "Sukces", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Form1 form2 = new Form1();
+                    form2.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Wystąpił problem podczas rejestracji użytkownika.", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Błąd: " + ex.Message, "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form1 form2 = new Form1();
+            form2.Show();
+            this.Hide();
+        }
+    }
+}
