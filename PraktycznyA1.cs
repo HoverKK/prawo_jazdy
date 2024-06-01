1970fd5777421580ce6e22e6bcb14ff934fe8e41
Jakub Konicki <147485931+HoverKK@users.noreply.github.com>
2024-06-01 21:46:29 +0100

    Egzamin PraktycznyA1

---
+++ PraktycznyA1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prawo_jazdy
+{
+    public partial class PraktycznyA1 : Form
+    {
+        public PraktycznyA1()
+        {
+            InitializeComponent();
+        }
+    }
+}
