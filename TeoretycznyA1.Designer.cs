4cab1f634f9af49d3ce073e0dfd659baac5f796d
Jakub Konicki <147485931+HoverKK@users.noreply.github.com>
2024-06-01 21:34:19 +0100

    Egzamin TeoretycznyA1

---
+++ TeoretycznyA1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace prawo_jazdy
+{
+    partial class TeoretycznyA1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.labelQuestion = new System.Windows.Forms.Label();
+            this.radioButtonA = new System.Windows.Forms.RadioButton();
+            this.radioButtonB = new System.Windows.Forms.RadioButton();
+            this.radioButtonC = new System.Windows.Forms.RadioButton();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelQuestion
+            // 
+            this.labelQuestion.AutoSize = true;
+            this.labelQuestion.Location = new System.Drawing.Point(13, 13);
+            this.labelQuestion.Name = "labelQuestion";
+            this.labelQuestion.Size = new System.Drawing.Size(50, 20);
+            this.labelQuestion.TabIndex = 0;
+            this.labelQuestion.Text = "Pytanie";
+            // 
+            // radioButtonA
+            // 
+            this.radioButtonA.AutoSize = true;
+            this.radioButtonA.Location = new System.Drawing.Point(17, 50);
+            this.radioButtonA.Name = "radioButtonA";
+            this.radioButtonA.Size = new System.Drawing.Size(85, 24);
+            this.radioButtonA.TabIndex = 1;
+            this.radioButtonA.TabStop = true;
+            this.radioButtonA.Text = "Odpowiedź A";
+            this.radioButtonA.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonB
+            // 
+            this.radioButtonB.AutoSize = true;
+            this.radioButtonB.Location = new System.Drawing.Point(17, 80);
+            this.radioButtonB.Name = "radioButtonB";
+            this.radioButtonB.Size = new System.Drawing.Size(85, 24);
+            this.radioButtonB.TabIndex = 2;
+            this.radioButtonB.TabStop = true;
+            this.radioButtonB.Text = "Odpowiedź B";
+            this.radioButtonB.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonC
+            // 
+            this.radioButtonC.AutoSize = true;
+            this.radioButtonC.Location = new System.Drawing.Point(17, 110);
+            this.radioButtonC.Name = "radioButtonC";
+            this.radioButtonC.Size = new System.Drawing.Size(85, 24);
+            this.radioButtonC.TabIndex = 3;
+            this.radioButtonC.TabStop = true;
+            this.radioButtonC.Text = "Odpowiedź C";
+            this.radioButtonC.UseVisualStyleBackColor = true;
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Location = new System.Drawing.Point(17, 140);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(85, 30);
+            this.buttonNext.TabIndex = 4;
+            this.buttonNext.Text = "Następne";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Location = new System.Drawing.Point(17, 180);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(120, 20);
+            this.labelTime.TabIndex = 5;
+            this.labelTime.Text = "Pozostały czas: 30 sekund";
+            // 
+            // TeoretycznyA1
+            // 
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelTime);
+            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(this.radioButtonC);
+            this.Controls.Add(this.radioButtonB);
+            this.Controls.Add(this.radioButtonA);
+            this.Controls.Add(this.labelQuestion);
+            this.Name = "TeoretycznyA1";
+            this.Load += new System.EventHandler(this.TeoretycznyA1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Label labelQuestion;
+        private System.Windows.Forms.RadioButton radioButtonA;
+        private System.Windows.Forms.RadioButton radioButtonB;
+        private System.Windows.Forms.RadioButton radioButtonC;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.Label labelTime;
+    }
+}
