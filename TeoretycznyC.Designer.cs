7a54e31b306fc2840e5bf3116224e2cb87f0549c
Jakub Konicki <147485931+HoverKK@users.noreply.github.com>
2024-06-02 13:40:17 +0100

    Egzamin teoretyczny C

---
+++ TeoretycznyC.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace prawo_jazdy
+{
+    partial class TeoretycznyC
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelQuestion = new System.Windows.Forms.Label();
+            this.radioButtonA = new System.Windows.Forms.RadioButton();
+            this.radioButtonB = new System.Windows.Forms.RadioButton();
+            this.radioButtonC = new System.Windows.Forms.RadioButton();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelQuestion
+            // 
+            this.labelQuestion.AutoSize = true;
+            this.labelQuestion.Location = new System.Drawing.Point(12, 20);
+            this.labelQuestion.Name = "labelQuestion";
+            this.labelQuestion.Size = new System.Drawing.Size(46, 17);
+            this.labelQuestion.TabIndex = 0;
+            this.labelQuestion.Text = "Pytanie";
+            // 
+            // radioButtonA
+            // 
+            this.radioButtonA.AutoSize = true;
+            this.radioButtonA.Location = new System.Drawing.Point(15, 50);
+            this.radioButtonA.Name = "radioButtonA";
+            this.radioButtonA.Size = new System.Drawing.Size(36, 17);
+            this.radioButtonA.TabIndex = 1;
+            this.radioButtonA.TabStop = true;
+            this.radioButtonA.Text = "A";
+            this.radioButtonA.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonB
+            // 
+            this.radioButtonB.AutoSize = true;
+            this.radioButtonB.Location = new System.Drawing.Point(15, 80);
+            this.radioButtonB.Name = "radioButtonB";
+            this.radioButtonB.Size = new System.Drawing.Size(36, 17);
+            this.radioButtonB.TabIndex = 2;
+            this.radioButtonB.TabStop = true;
+            this.radioButtonB.Text = "B";
+            this.radioButtonB.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonC
+            // 
+            this.radioButtonC.AutoSize = true;
+            this.radioButtonC.Location = new System.Drawing.Point(15, 110);
+            this.radioButtonC.Name = "radioButtonC";
+            this.radioButtonC.Size = new System.Drawing.Size(36, 17);
+            this.radioButtonC.TabIndex = 3;
+            this.radioButtonC.TabStop = true;
+            this.radioButtonC.Text = "C";
+            this.radioButtonC.UseVisualStyleBackColor = true;
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Location = new System.Drawing.Point(100, 140);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(75, 23);
+            this.buttonNext.TabIndex = 4;
+            this.buttonNext.Text = "Dalej";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Location = new System.Drawing.Point(200, 20);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(96, 17);
+            this.labelTime.TabIndex = 5;
+            this.labelTime.Text = "Pozostały czas:";
+            // 
+            // TeoretycznyC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 200);
+            this.Controls.Add(this.labelTime);
+            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(this.radioButtonC);
+            this.Controls.Add(this.radioButtonB);
+            this.Controls.Add(this.radioButtonA);
+            this.Controls.Add(this.labelQuestion);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "TeoretycznyC";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TeoretycznyC";
+            this.Load += new System.EventHandler(this.TeoretycznyC_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelQuestion;
+        private System.Windows.Forms.RadioButton radioButtonA;
+        private System.Windows.Forms.RadioButton radioButtonB;
+        private System.Windows.Forms.RadioButton radioButtonC;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.Label labelTime;
+    }
+}
---
+++ TeoretycznyC.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace prawo_jazdy
+{
+    public partial class TeoretycznyC : Form
+    {
+        private int currentQuestionIndex = 0;
+        private int timeLeft = 30;
+        private System.Windows.Forms.Timer timer;
+        private int correctAnswersCount = 0;
+        private int totalPoints = 0;
+        private int maxPoints = 0;
+        private List<QuestionC> questions = new List<QuestionC>();
+        private List<QuestionC> selectedQuestions = new List<QuestionC>();
+
+        private string connectionString = "your_connection_string_here";
+
+        public TeoretycznyC()
+        {
+            InitializeComponent();
+            LoadQuestionsFromDatabase();
+            SelectRandomQuestions();
+            InitializeTimer();
+            DisplayQuestion();
+        }
+
+        private void LoadQuestionsFromDatabase()
+        {
+            using (SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-ED41F2S;Initial Catalog=Prawo_jazdy;Integrated Security=True"))
+            {
+                connection.Open();
+                string query = "SELECT p.PytanieID, p.PytanieText, o.OdpowiedzID, o.OdpowiedzText, o.Poprawne, o.Punkty " +
+                               "FROM Pytania p " +
+                               "JOIN Odpowiedzi o ON p.PytanieID = o.PytanieID";
+
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                Dictionary<int, QuestionC> questionDictionary = new Dictionary<int, QuestionC>();
+
+                while (reader.Read())
+                {
+                    int questionId = reader.GetInt32(0);
+                    string questionText = reader.GetString(1);
+                    int answerId = reader.GetInt32(2);
+                    string answerText = reader.GetString(3);
+                    bool isCorrect = reader.GetBoolean(4);
+                    int points = reader.GetInt32(5);
+
+                    if (!questionDictionary.ContainsKey(questionId))
+                    {
+                        questionDictionary[questionId] = new QuestionC
+                        {
+                            QuestionID = questionId,
+                            QuestionText = questionText,
+                            Points = points,
+                            Answers = new List<AnswerC>()
+                        };
+                    }
+
+                    questionDictionary[questionId].Answers.Add(new AnswerC
+                    {
+                        AnswerID = answerId,
+                        AnswerText = answerText,
+                        IsCorrect = isCorrect
+                    });
+                }
+
+                questions.AddRange(questionDictionary.Values);
+                reader.Close();
+            }
+        }
+
+        private void SelectRandomQuestions()
+        {
+            Random random = new Random();
+            var shuffledQuestions = questions.OrderBy(q => random.Next()).ToList();
+            int sumPoints = 0;
+            foreach (var question in shuffledQuestions)
+            {
+                if (sumPoints + question.Points <= 74)
+                {
+                    selectedQuestions.Add(question);
+                    sumPoints += question.Points;
+                }
+                if (sumPoints == 74) break;
+            }
+            maxPoints = sumPoints;
+        }
+
+        private void InitializeTimer()
+        {
+            timer = new System.Windows.Forms.Timer();
+            timer.Interval = 1000;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (timeLeft > 0)
+            {
+                timeLeft--;
+                labelTime.Text = $"Pozostały czas: {timeLeft} sekund";
+            }
+            else
+            {
+                timer.Stop();
+                CheckAnswer();
+                GoToNextQuestion();
+            }
+        }
+
+        private void DisplayQuestion()
+        {
+            timeLeft = 30;
+            labelTime.Text = $"Pozostały czas: {timeLeft} sekund";
+            timer.Start();
+
+            if (selectedQuestions.Count > 0 && currentQuestionIndex < selectedQuestions.Count)
+            {
+                var currentQuestion = selectedQuestions[currentQuestionIndex];
+                labelQuestion.Text = $"{currentQuestionIndex + 1}. {currentQuestion.QuestionText}";
+                radioButtonA.Text = currentQuestion.Answers[0].AnswerText;
+                radioButtonB.Text = currentQuestion.Answers[1].AnswerText;
+                radioButtonC.Text = currentQuestion.Answers[2].AnswerText;
+            }
+            else
+            {
+                MessageBox.Show("Brak pytań do wyświetlenia!");
+            }
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            CheckAnswer();
+            GoToNextQuestion();
+        }
+
+        private void CheckAnswer()
+        {
+            if (selectedQuestions.Count > 0 && currentQuestionIndex < selectedQuestions.Count)
+            {
+                var currentQuestion = selectedQuestions[currentQuestionIndex];
+                var checkedRadioButton = this.Controls.OfType<RadioButton>().FirstOrDefault(r => r.Checked);
+                string selectedAnswerText = checkedRadioButton != null ? checkedRadioButton.Text : "Brak odpowiedzi";
+
+                currentQuestion.UserSelectedAnswer = selectedAnswerText;
+
+                var userAnswer = currentQuestion.Answers.FirstOrDefault(a => a.AnswerText == selectedAnswerText);
+                if (userAnswer != null && userAnswer.IsCorrect)
+                {
+                    correctAnswersCount++;
+                    totalPoints += currentQuestion.Points;
+                }
+
+                radioButtonA.Checked = false;
+                radioButtonB.Checked = false;
+                radioButtonC.Checked = false;
+            }
+        }
+
+        private void GoToNextQuestion()
+        {
+            if (selectedQuestions.Count > 0 && currentQuestionIndex < selectedQuestions.Count - 1)
+            {
+                currentQuestionIndex++;
+                DisplayQuestion();
+            }
+            else
+            {
+                timer.Stop();
+                ShowResult();
+            }
+        }
+
+        private void ShowResult()
+        {
+            var resultForm = new ResultFormC(correctAnswersCount, selectedQuestions.Count, totalPoints, maxPoints, selectedQuestions);
+            resultForm.Show();
+            this.Close();
+        }
+
+        private void TeoretycznyC_Load(object sender, EventArgs e)
+        {
+            DisplayQuestion();
+        }
+    }
+}
+
+public class QuestionC
+{
+    public int QuestionID { get; set; }
+    public string QuestionText { get; set; }
+    public int Points { get; set; }
+    public List<AnswerC> Answers { get; set; }
+    public string UserSelectedAnswer { get; set; }
+}
+
+public class AnswerC
+{
+    public int AnswerID { get; set; }
+    public string AnswerText { get; set; }
+    public bool IsCorrect { get; set; }
+}
+
+public class ResultFormC : Form
+{
+    public ResultFormC(int correctAnswersCount, int totalQuestions, int totalPoints, int maxPoints, List<QuestionC> questions)
+    {
+        this.Text = "Wynik";
+        this.Size = new Size(1600, 1100);
+
+        Label resultLabel = new Label();
+        resultLabel.Text = $"Uzyskane punkty: {totalPoints} / {maxPoints}";
+        resultLabel.Location = new Point(10, 10);
+        resultLabel.AutoSize = true;
+        this.Controls.Add(resultLabel);
+
+        Label correctAnswersLabel = new Label();
+        correctAnswersLabel.Text = $"Poprawne odpowiedzi: {correctAnswersCount} / {totalQuestions}";
+        correctAnswersLabel.Location = new Point(10, 40);
+        correctAnswersLabel.AutoSize = true;
+        this.Controls.Add(correctAnswersLabel);
+
+        Label passLabel = new Label();
+        passLabel.Location = new Point(10, 70);
+        passLabel.AutoSize = true;
+        if (totalPoints >= 68) // Prog zdawalności
+        {
+            passLabel.Text = "Zdałeś!";
+            passLabel.ForeColor = Color.Green;
+        }
+        else
+        {
+            passLabel.Text = "Nie zdałeś!";
+            passLabel.ForeColor = Color.Red;
+        }
+        this.Controls.Add(passLabel);
+
+        ListView listView = new ListView();
+        listView.View = View.Details;
+        listView.Columns.Add("Pytanie", 600);
+        listView.Columns.Add("Twoja odpowiedź", 450);
+        listView.Columns.Add("Poprawna odpowiedź", 450);
+        listView.FullRowSelect = true;
+        listView.GridLines = true;
+        listView.Location = new Point(10, 100);
+        listView.Size = new Size(1600, 800);
+
+        foreach (var question in questions)
+        {
+            var userAnswer = question.Answers.FirstOrDefault(a => a.AnswerText == question.UserSelectedAnswer);
+            var correctAnswer = question.Answers.FirstOrDefault(a => a.IsCorrect);
+
+            ListViewItem item = new ListViewItem(question.QuestionText);
+            item.SubItems.Add(userAnswer != null ? userAnswer.AnswerText : "Brak odpowiedzi");
+            item.SubItems.Add(correctAnswer.AnswerText);
+            listView.Items.Add(item);
+        }
+
+        this.Controls.Add(listView);
+
+        Button closeButton = new Button();
+        closeButton.Text = "Zamknij";
+        closeButton.Location = new Point(700, 920);
+        closeButton.Size = new Size(100, 40);
+        closeButton.Click += (s, e) => this.Close();
+        this.Controls.Add(closeButton);
+    }
+}
+
+
+
+
+
