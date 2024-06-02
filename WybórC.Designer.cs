b9228ad63bfd984cb2d17c2c336d08e404cbc56e
Jakub Konicki <147485931+HoverKK@users.noreply.github.com>
2024-06-02 13:38:08 +0100

    Wybór kategorii C

---
+++ WybórC.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace prawo_jazdy
+{
+    partial class WybórC
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(436, 238);
+            button1.Margin = new Padding(4, 5, 4, 5);
+            button1.Name = "button1";
+            button1.Size = new Size(233, 77);
+            button1.TabIndex = 0;
+            button1.Text = "Teoretyczny";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(436, 345);
+            button2.Margin = new Padding(4, 5, 4, 5);
+            button2.Name = "button2";
+            button2.Size = new Size(233, 82);
+            button2.TabIndex = 1;
+            button2.Text = "Praktyczny";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(33, 604);
+            button3.Margin = new Padding(4, 5, 4, 5);
+            button3.Name = "button3";
+            button3.Size = new Size(155, 56);
+            button3.TabIndex = 2;
+            button3.Text = "Cofnij";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // WybórC
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1067, 692);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Margin = new Padding(4, 5, 4, 5);
+            Name = "WybórC";
+            Text = "Wybór egzaminu";
+            Load += WybórC_Load;
+            ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ WybórC.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prawo_jazdy
+{
+    public partial class WybórC: Form
+    {
+        public WybórC()
+        {
+            InitializeComponent();
+        }
+
+        private void WybórC_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TeoretycznyC form = new TeoretycznyC();
+            form.Show();
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Praktyczny form = new Praktyczny();
+            form.Show();
+            this.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Menuform form1 = new Menuform();
+            form1.Show();
+            this.Hide();
+        }
+    }
+}
