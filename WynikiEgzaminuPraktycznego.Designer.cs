a5231202a7fe0ee7f7cf8c8355ffda9fdb4dfde5
MaxenWolf <147586449+MaxenWolf@users.noreply.github.com>
2024-06-23 20:48:07 +0200

    Wyniki egzaminu praktycznego

---
+++ WynikiEgzaminuPraktycznego.Designer.cs
@@ -0,0 +1,41 @@
+﻿namespace prawo_jazdy
+{
+    partial class WynikiEgzaminuPraktycznego
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.DataGridView dataGridView1;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 426);
+            this.dataGridView1.TabIndex = 0;
+            
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "WynikiEgzaminuPraktycznego";
+            this.Text = "Wyniki Egzaminu Praktycznego";
+            this.Load += new System.EventHandler(this.WynikiEgzaminuPraktycznego_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+    }
+}
---
+++ WynikiEgzaminuPraktycznego.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace prawo_jazdy
+{
+    public partial class WynikiEgzaminuPraktycznego : Form
+    {
+        public WynikiEgzaminuPraktycznego()
+        {
+            InitializeComponent();
+            this.FormClosed += WynikiEgzaminuPraktycznego_FormClosed; 
+        }
+
+        private void WynikiEgzaminuPraktycznego_Load(object sender, EventArgs e)
+        {
+            LoadWyniki();
+        }
+
+        private void LoadWyniki()
+        {
+            string connectionString = "your_connection_string_here"; // Zastąp odpowiednim connection stringiem
+            string query = "SELECT TOP (1000) [Id], [Name], [LastName], [ExamDate], [StartTime], [EndTime], [Task], [Points] FROM [Prawo_jazdy].[dbo].[ExamResults]";
+
+            using (SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-ED41F2S;Initial Catalog=Prawo_jazdy;Integrated Security=True"))
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                dataGridView1.DataSource = dataTable;
+            }
+        }
+
+        private void WynikiEgzaminuPraktycznego_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            PanelAdmina panelAdmina = new PanelAdmina();
+            panelAdmina.Show();
+        }
+    }
+}
+
