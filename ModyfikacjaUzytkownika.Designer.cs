be5040d028eb04414b3b84c835065d67126599ff
Jakub Konicki <147485931+HoverKK@users.noreply.github.com>
2024-06-23 18:17:33 +0100

    Modyfikowanie użytkownika
    Przez Admina

---
+++ ModyfikacjaUzytkownika.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace prawo_jazdy
+{
+    partial class ModyfikacjaUzytkownika
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.Label lblImie;
+        private System.Windows.Forms.Label lblNazwisko;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblHaslo;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtImie;
+        private System.Windows.Forms.TextBox txtNazwisko;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtHaslo;
+        private System.Windows.Forms.Button btnZapisz;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.lblId = new System.Windows.Forms.Label();
+            this.lblImie = new System.Windows.Forms.Label();
+            this.lblNazwisko = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblHaslo = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtImie = new System.Windows.Forms.TextBox();
+            this.txtNazwisko = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtHaslo = new System.Windows.Forms.TextBox();
+            this.btnZapisz = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(50, 20);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(21, 13);
+            this.lblId.TabIndex = 0;
+            this.lblId.Text = "ID:";
+            // 
+            // lblImie
+            // 
+            this.lblImie.AutoSize = true;
+            this.lblImie.Location = new System.Drawing.Point(50, 50);
+            this.lblImie.Name = "lblImie";
+            this.lblImie.Size = new System.Drawing.Size(26, 13);
+            this.lblImie.TabIndex = 1;
+            this.lblImie.Text = "Imię:";
+            // 
+            // lblNazwisko
+            // 
+            this.lblNazwisko.AutoSize = true;
+            this.lblNazwisko.Location = new System.Drawing.Point(50, 80);
+            this.lblNazwisko.Name = "lblNazwisko";
+            this.lblNazwisko.Size = new System.Drawing.Size(56, 13);
+            this.lblNazwisko.TabIndex = 2;
+            this.lblNazwisko.Text = "Nazwisko:";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(50, 110);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(38, 13);
+            this.lblEmail.TabIndex = 3;
+            this.lblEmail.Text = "E-mail:";
+            // 
+            // lblHaslo
+            // 
+            this.lblHaslo.AutoSize = true;
+            this.lblHaslo.Location = new System.Drawing.Point(50, 140);
+            this.lblHaslo.Name = "lblHaslo";
+            this.lblHaslo.Size = new System.Drawing.Size(39, 13);
+            this.lblHaslo.TabIndex = 4;
+            this.lblHaslo.Text = "Hasło:";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(150, 20);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(200, 20);
+            this.txtId.TabIndex = 5;
+            // 
+            // txtImie
+            // 
+            this.txtImie.Location = new System.Drawing.Point(150, 50);
+            this.txtImie.Name = "txtImie";
+            this.txtImie.Size = new System.Drawing.Size(200, 20);
+            this.txtImie.TabIndex = 6;
+            // 
+            // txtNazwisko
+            // 
+            this.txtNazwisko.Location = new System.Drawing.Point(150, 80);
+            this.txtNazwisko.Name = "txtNazwisko";
+            this.txtNazwisko.Size = new System.Drawing.Size(200, 20);
+            this.txtNazwisko.TabIndex = 7;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(150, 110);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(200, 20);
+            this.txtEmail.TabIndex = 8;
+            // 
+            // txtHaslo
+            // 
+            this.txtHaslo.Location = new System.Drawing.Point(150, 140);
+            this.txtHaslo.Name = "txtHaslo";
+            this.txtHaslo.Size = new System.Drawing.Size(200, 20);
+            this.txtHaslo.TabIndex = 9;
+            this.txtHaslo.PasswordChar = '*';
+            // 
+            // btnZapisz
+            // 
+            this.btnZapisz.Location = new System.Drawing.Point(150, 180);
+            this.btnZapisz.Name = "btnZapisz";
+            this.btnZapisz.Size = new System.Drawing.Size(100, 30);
+            this.btnZapisz.TabIndex = 10;
+            this.btnZapisz.Text = "Zapisz";
+            this.btnZapisz.UseVisualStyleBackColor = true;
+            this.btnZapisz.Click += new System.EventHandler(this.btnZapisz_Click);
+            // 
+            // ModyfikacjaUzytkownika
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 250);
+            this.Controls.Add(this.btnZapisz);
+            this.Controls.Add(this.txtHaslo);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtNazwisko);
+            this.Controls.Add(this.txtImie);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.lblHaslo);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblNazwisko);
+            this.Controls.Add(this.lblImie);
+            this.Controls.Add(this.lblId);
+            this.Name = "ModyfikacjaUzytkownika";
+            this.Text = "Modyfikacja Użytkownika";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ ModyfikacjaUzytkownika.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace prawo_jazdy
+{
+    public partial class ModyfikacjaUzytkownika : Form
+    {
+        private SqlConnection conn;
+
+        public ModyfikacjaUzytkownika()
+        {
+            InitializeComponent();
+            conn = new SqlConnection(@"Data Source=DESKTOP-ED41F2S;Initial Catalog=Prawo_jazdy;Integrated Security=True");
+            this.FormClosed += ModyfikacjaUzytkownika_FormClosed; 
+        }
+
+        private void btnZapisz_Click(object sender, EventArgs e)
+        {
+            string id = txtId.Text;
+            string imie = txtImie.Text;
+            string nazwisko = txtNazwisko.Text;
+            string email = txtEmail.Text;
+            string haslo = txtHaslo.Text;
+
+            if (string.IsNullOrWhiteSpace(id) || string.IsNullOrWhiteSpace(imie) || string.IsNullOrWhiteSpace(nazwisko) || string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(haslo))
+            {
+                MessageBox.Show("Wszystkie pola są wymagane.", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                conn.Open();
+                string query = "UPDATE urzytkownik SET imię = @imie, nazwisko = @nazwisko, email = @email, hasło = @haslo WHERE id = @id";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.Parameters.AddWithValue("@imie", imie);
+                cmd.Parameters.AddWithValue("@nazwisko", nazwisko);
+                cmd.Parameters.AddWithValue("@email", email);
+                cmd.Parameters.AddWithValue("@haslo", haslo);
+                int rowsAffected = cmd.ExecuteNonQuery();
+
+                if (rowsAffected > 0)
+                {
+                    MessageBox.Show("Dane użytkownika zostały pomyślnie zmodyfikowane.", "Sukces", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Wystąpił problem podczas modyfikacji danych użytkownika.", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Błąd: " + ex.Message, "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void ModyfikacjaUzytkownika_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            PanelAdmina panelAdmina = new PanelAdmina();
+            panelAdmina.Show();
+        }
+    }
+}
