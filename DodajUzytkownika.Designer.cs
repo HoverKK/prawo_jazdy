2551139498c618280ff898b223822f0f0428f64f
Jakub Konicki <147485931+HoverKK@users.noreply.github.com>
2024-06-23 18:16:49 +0100

    Dodawanie użytkownika
    Przez Admina

---
+++ DodajUzytkownika.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace prawo_jazdy
+{
+    partial class DodajUzytkownika
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox txtImie;
+        private System.Windows.Forms.TextBox txtNazwisko;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtHaslo;
+        private System.Windows.Forms.Label lblImie;
+        private System.Windows.Forms.Label lblNazwisko;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblHaslo;
+        private System.Windows.Forms.Button btnDodaj;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtImie = new System.Windows.Forms.TextBox();
+            this.txtNazwisko = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtHaslo = new System.Windows.Forms.TextBox();
+            this.lblImie = new System.Windows.Forms.Label();
+            this.lblNazwisko = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblHaslo = new System.Windows.Forms.Label();
+            this.btnDodaj = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtImie
+            // 
+            this.txtImie.Location = new System.Drawing.Point(150, 50);
+            this.txtImie.Name = "txtImie";
+            this.txtImie.Size = new System.Drawing.Size(200, 20);
+            this.txtImie.TabIndex = 0;
+            // 
+            // txtNazwisko
+            // 
+            this.txtNazwisko.Location = new System.Drawing.Point(150, 90);
+            this.txtNazwisko.Name = "txtNazwisko";
+            this.txtNazwisko.Size = new System.Drawing.Size(200, 20);
+            this.txtNazwisko.TabIndex = 1;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(150, 130);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(200, 20);
+            this.txtEmail.TabIndex = 2;
+            // 
+            // txtHaslo
+            // 
+            this.txtHaslo.Location = new System.Drawing.Point(150, 170);
+            this.txtHaslo.Name = "txtHaslo";
+            this.txtHaslo.Size = new System.Drawing.Size(200, 20);
+            this.txtHaslo.TabIndex = 3;
+            this.txtHaslo.PasswordChar = '*';
+            // 
+            // lblImie
+            // 
+            this.lblImie.AutoSize = true;
+            this.lblImie.Location = new System.Drawing.Point(50, 53);
+            this.lblImie.Name = "lblImie";
+            this.lblImie.Size = new System.Drawing.Size(29, 13);
+            this.lblImie.TabIndex = 4;
+            this.lblImie.Text = "Imię:";
+            // 
+            // lblNazwisko
+            // 
+            this.lblNazwisko.AutoSize = true;
+            this.lblNazwisko.Location = new System.Drawing.Point(50, 93);
+            this.lblNazwisko.Name = "lblNazwisko";
+            this.lblNazwisko.Size = new System.Drawing.Size(56, 13);
+            this.lblNazwisko.TabIndex = 5;
+            this.lblNazwisko.Text = "Nazwisko:";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(50, 133);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(38, 13);
+            this.lblEmail.TabIndex = 6;
+            this.lblEmail.Text = "E-mail:";
+            // 
+            // lblHaslo
+            // 
+            this.lblHaslo.AutoSize = true;
+            this.lblHaslo.Location = new System.Drawing.Point(50, 173);
+            this.lblHaslo.Name = "lblHaslo";
+            this.lblHaslo.Size = new System.Drawing.Size(39, 13);
+            this.lblHaslo.TabIndex = 7;
+            this.lblHaslo.Text = "Hasło:";
+            // 
+            // btnDodaj
+            // 
+            this.btnDodaj.Location = new System.Drawing.Point(150, 210);
+            this.btnDodaj.Name = "btnDodaj";
+            this.btnDodaj.Size = new System.Drawing.Size(200, 30);
+            this.btnDodaj.TabIndex = 8;
+            this.btnDodaj.Text = "Dodaj użytkownika";
+            this.btnDodaj.UseVisualStyleBackColor = true;
+            this.btnDodaj.Click += new System.EventHandler(this.btnDodaj_Click);
+            // 
+            // DodajUzytkownika
+            // 
+            this.ClientSize = new System.Drawing.Size(400, 300);
+            this.Controls.Add(this.btnDodaj);
+            this.Controls.Add(this.lblHaslo);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblNazwisko);
+            this.Controls.Add(this.lblImie);
+            this.Controls.Add(this.txtHaslo);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtNazwisko);
+            this.Controls.Add(this.txtImie);
+            this.Name = "DodajUzytkownika";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ DodajUzytkownika.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace prawo_jazdy
+{
+    public partial class DodajUzytkownika : Form
+    {
+        private SqlConnection conn;
+
+        public DodajUzytkownika()
+        {
+            InitializeComponent();
+            conn = new SqlConnection(@"Data Source=DESKTOP-ED41F2S;Initial Catalog=Prawo_jazdy;Integrated Security=True");
+            this.FormClosed += DodajUzytkownika_FormClosed;
+        }
+        private void btnDodaj_Click(object sender, EventArgs e)
+        {
+            string imie = txtImie.Text;
+            string nazwisko = txtNazwisko.Text;
+            string email = txtEmail.Text;
+            string haslo = txtHaslo.Text;
+
+            if (string.IsNullOrWhiteSpace(imie) || string.IsNullOrWhiteSpace(nazwisko) || string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(haslo))
+            {
+                MessageBox.Show("Wszystkie pola są wymagane.", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                conn.Open();
+
+
+                string query = "INSERT INTO urzytkownik (imię, nazwisko, email, hasło) VALUES (@imie, @nazwisko, @email, @haslo)";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@imie", imie);
+                cmd.Parameters.AddWithValue("@nazwisko", nazwisko);
+                cmd.Parameters.AddWithValue("@email", email);
+                cmd.Parameters.AddWithValue("@haslo", haslo);
+
+                int rowsAffected = cmd.ExecuteNonQuery();
+
+                if (rowsAffected > 0)
+                {
+                    MessageBox.Show("Użytkownik został dodany.", "Sukces", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    this.Close(); 
+                }
+                else
+                {
+                    MessageBox.Show("Wystąpił problem podczas dodawania użytkownika.", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Błąd: " + ex.Message, "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void DodajUzytkownika_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            PanelAdmina panelAdmina = new PanelAdmina();
+            panelAdmina.Show();
+        }
+    }
+}
