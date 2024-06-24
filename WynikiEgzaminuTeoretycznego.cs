36d06d15d4cda291764cca2082d1e96ebe3637ca
MaxenWolf <147586449+MaxenWolf@users.noreply.github.com>
2024-06-24 11:42:54 +0200

    Wyniki Egzaminu Teoretycznego

---
+++ WynikiEgzaminuTeoretycznego.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace prawo_jazdy
+{
+    public partial class WynikiEgzaminuTeoretycznego : Form
+    {
+        public WynikiEgzaminuTeoretycznego()
+        {
+            InitializeComponent();
+            this.FormClosed += WynikiEgzaminuTeoretycznego_FormClosed; 
+        }
+
+        private void WynikiEgzaminuTeoretycznego_Load(object sender, EventArgs e)
+        {
+            LoadWyniki();
+        }
+
+        private void LoadWyniki()
+        {
+            string connectionString = "your_connection_string_here"; 
+            string query = "SELECT TOP (1000) [WynikID], [TotalPoints], [MaxPoints], [CorrectAnswersCount], [TotalQuestions], [Timestamp], [Kategoria] FROM [Prawo_jazdy].[dbo].[Wyniki]";
+
+            using (SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-ED41F2S;Initial Catalog=Prawo_jazdy;Integrated Security=True"))
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                dataGridView1.DataSource = dataTable;
+            }
+        }
+
+        private void WynikiEgzaminuTeoretycznego_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            PanelAdmina panelAdmina = new PanelAdmina();
+            panelAdmina.Show();
+        }
+    }
+}
